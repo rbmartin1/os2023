64b256dd470656200466b6dff32ab1f9799e862b
Martin Vidlicka <martin.vidlicka@gmail.com>
2023-11-02 10:28:03 +0100

    aaa

--- kernel/vm.c
+++ kernel/vm.c
@@ -324,11 +324,12 @@ uvmcopy(pagetable_t old, pagetable_t new, uint64 sz)
       panic("uvmcopy: page not present");
     pa = PTE2PA(*pte);
     flags = PTE_FLAGS(*pte);
-    if((mem = kalloc()) == 0)
-      goto err;
+
+    
+
     memmove(mem, (char*)pa, PGSIZE);
     if(mappages(new, i, PGSIZE, (uint64)mem, flags) != 0){
-      kfree(mem);
+      //kfree(mem);
       goto err;
     }
   }
